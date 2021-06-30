cf4b0114deba66120422a2bcce992f88d142a2e8
Diego21605 <54678330+Diego21605@users.noreply.github.com>
2021-06-30 09:54:15 -0500

    Add files via upload

---
+++ operadores aritmeticos.php
@@ -0,0 +1,22 @@
+<?php 
+
+	/*Operadores Aritmeticos
+
+		+  Suma
+		-  Resta
+		*  Multiplicacion
+		/  Division
+		** Exponenciacion
+		%  Residuo
+
+	*/
+
+		$x = 100;
+		$y = 2;
+
+		$resultados = $x % $y;
+
+		echo $resultados;
+
+		
+ ?>
---
+++ operadores de cadenas.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*		OPERADORES DE CADENAS O STRING
+
+		. 	Concatenacion
+		.=  Asignacion de concatenacion
+	*/
+
+
+ $cad1 = "El sol es amarillo";
+ $cad2 = " La luna es gris";
+
+ $union  = $cad1 ." y " . $cad2;
+
+$cad3 = " El universo es marivilloso";
+$union .= $cad3;
+
+ echo $union;
+
+ ?>
